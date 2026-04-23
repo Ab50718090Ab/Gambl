c7717857858f5f183f9ec4d08614843e93578553
Ab50718090Ab <mithuray8708@gmail.com>
2026-04-23 22:40:03 +0600

    Delete server directory

--- server/utils/generateOTP.js
+++
@@ -1,4 +0,0 @@
-export const generateOTP = () => {
-    const otp = String(Math.floor(100000 + Math.random() * 900000))
-    return otp
-}
--- server/utils/generateVerificationToken.js
+++
@@ -1,8 +0,0 @@
-import crypto from 'crypto';
-
-// Generate a unique verification token
-const generateVerificationToken = () => {
-    return crypto.randomBytes(20).toString('hex');
-};
-
-export default generateVerificationToken
